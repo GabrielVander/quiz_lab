8f9eed8e72e8f97c438c1ba99ebdd864f411e6d4
Gabriel Vander <gabsvander@hotmail.com>
2022-07-07 21:22:39 -0300

    quiz_lab-2 Basic folder structure setup

--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:quiz_lab/main.dart';
+import 'package:quiz_lab/core/quiz_lab_application.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const QuizLabApplication());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
