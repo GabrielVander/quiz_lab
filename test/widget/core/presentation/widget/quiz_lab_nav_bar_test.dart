239ffa547bc7c37c7d871dd0bc9de9deedc3c126
Gabriel Vander dos Santos Guimarães <gabsvander@hotmail.com>
2022-08-21 15:28:45 -0300

    quiz_lab-3 Fixed Unit Tests

--- test/widget/core/presentation/widget/quiz_lab_nav_bar_test.dart
+++ test/widget/core/presentation/widget/quiz_lab_nav_bar_test.dart
@@ -1,37 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:quiz_lab/core/quiz_lab_application.dart';
+import 'package:quiz_lab/core/themes/light_theme.dart';
 import 'package:quiz_lab/core/widgets/quiz_lab_nav_bar.dart';
 
 void main() {
   group('Should render', () {
     testWidgets('Nav bar it self', (WidgetTester tester) async {
-      await tester.pumpWidget(QuizLabApplication());
+      await tester.pumpWidget(_buildWidgetUnderTest());
 
       expect(find.byType(QuizLabNavBar), findsOneWidget);
       expect(find.byKey(const ValueKey<String>('navBar')), findsOneWidget);
     });
 
     testWidgets('Assessments', (WidgetTester tester) async {
-      await tester.pumpWidget(QuizLabApplication());
+      await tester.pumpWidget(_buildWidgetUnderTest());
 
-      expect(find.byIcon(MdiIcons.schoolOutline), findsOneWidget);
       expect(find.text('Assessments'), findsOneWidget);
     });
 
     testWidgets('Questions', (WidgetTester tester) async {
-      await tester.pumpWidget(QuizLabApplication());
+      await tester.pumpWidget(_buildWidgetUnderTest());
 
-      expect(find.byIcon(MdiIcons.database), findsOneWidget);
       expect(find.text('Questions'), findsOneWidget);
     });
 
     testWidgets('Results', (WidgetTester tester) async {
-      await tester.pumpWidget(QuizLabApplication());
+      await tester.pumpWidget(_buildWidgetUnderTest());
 
-      expect(find.byIcon(MdiIcons.cardBulleted), findsOneWidget);
       expect(find.text('Results'), findsOneWidget);
     });
   });
+}
+
+Widget _buildWidgetUnderTest() {
+  return MaterialApp(
+    theme: lightTheme,
+    home: const Scaffold(
+      bottomNavigationBar: QuizLabNavBar(
+        key: ValueKey<String>('navBar'),
+      ),
+    ),
+  );
 }
