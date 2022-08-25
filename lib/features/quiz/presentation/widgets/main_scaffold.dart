f543a672beae1924dfbfe22bc1f52fa1a0b8c9a9
Gabriel Vander <gabriel.guimaraes@monitoratec.com.br>
2022-08-24 21:03:34 -0300

    quiz_lab-21 Moved MainScaffold widget

--- lib/features/quiz/presentation/widgets/main_scaffold.dart
+++ lib/features/quiz/presentation/widgets/main_scaffold.dart
@@ -4,11 +4,10 @@ import 'package:hooked_bloc/hooked_bloc.dart';
 import 'package:quiz_lab/core/utils/dependency_injection/dependency_injection.dart';
 import 'package:quiz_lab/core/utils/responsiveness_utils/breakpoint.dart';
 import 'package:quiz_lab/core/utils/responsiveness_utils/screen_breakpoints.dart';
+import 'package:quiz_lab/features/quiz/presentation/manager/bottom_navigation/bottom_navigation_cubit.dart';
 import 'package:quiz_lab/features/quiz/presentation/pages/questions_page.dart';
 import 'package:quiz_lab/features/quiz/presentation/widgets/quiz_lab_app_bar.dart';
 import 'package:quiz_lab/features/quiz/presentation/widgets/quiz_lab_nav_bar.dart';
-
-import '../manager/bottom_navigation/bottom_navigation_cubit.dart';
 
 class MainScaffold extends StatelessWidget {
   const MainScaffold({
