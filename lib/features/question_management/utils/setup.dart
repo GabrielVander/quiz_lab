03f9cfc87cbada832779b9a57dc04adcb4f8fe2d
gabriel <gabsvander@hotmail.com>
2023-01-08 22:46:29 -0300

    QL-9 Updated UseCaseFactory to receive dependencies directly

--- lib/features/question_management/utils/setup.dart
+++ lib/features/question_management/utils/setup.dart
@@ -1,6 +1,7 @@
 import 'package:hive/hive.dart';
 import 'package:quiz_lab/core/presentation/manager/assessments_overview/assessments_overview_cubit.dart';
 import 'package:quiz_lab/core/utils/dependency_injection/dependency_injection.dart';
+import 'package:quiz_lab/core/utils/logger/logger.dart';
 import 'package:quiz_lab/core/utils/resource_uuid_generator.dart';
 import 'package:quiz_lab/features/question_management/data/data_sources/factories/data_source_factory.dart';
 import 'package:quiz_lab/features/question_management/data/repositories/factories/repository_factory_impl.dart';
@@ -9,7 +10,11 @@ import 'package:quiz_lab/features/question_management/data/repositories/question
 import 'package:quiz_lab/features/question_management/domain/repositories/factories/repository_factory.dart';
 import 'package:quiz_lab/features/question_management/domain/repositories/question_repository.dart';
 import 'package:quiz_lab/features/question_management/domain/use_cases/create_question_use_case.dart';
+import 'package:quiz_lab/features/question_management/domain/use_cases/delete_question_use_case.dart';
 import 'package:quiz_lab/features/question_management/domain/use_cases/factories/use_case_factory.dart';
+import 'package:quiz_lab/features/question_management/domain/use_cases/get_single_question_use_case.dart';
+import 'package:quiz_lab/features/question_management/domain/use_cases/update_question_use_case.dart';
+import 'package:quiz_lab/features/question_management/domain/use_cases/watch_all_questions_use_case.dart';
 import 'package:quiz_lab/features/question_management/presentation/managers/factories/question_management_cubit_factory.dart';
 import 'package:quiz_lab/features/question_management/presentation/managers/questions_overview/mappers/factories/presentation_mapper_factory.dart';
 import 'package:uuid/uuid.dart';
@@ -25,15 +30,41 @@ void questionManagementDiSetup(DependencyInjection di) {
     ..registerBuilder<RepositoryFactory>(
       (DependencyInjection di) => RepositoryFactoryImpl(),
     )
+    ..registerBuilder(
+      (di) => WatchAllQuestionsUseCase(
+        logger: di.get<Logger>(),
+        repositoryFactory: di.get<RepositoryFactory>(),
+      ),
+    )
     ..registerBuilder<CreateQuestionUseCase>(
       (DependencyInjection di) => CreateQuestionUseCase(
         uuidGenerator: const ResourceUuidGenerator(uuid: Uuid()),
         repositoryFactory: di.get<RepositoryFactory>(),
       ),
     )
+    ..registerBuilder<UpdateQuestionUseCase>(
+      (DependencyInjection di) => UpdateQuestionUseCase(
+        repositoryFactory: di.get<RepositoryFactory>(),
+      ),
+    )
+    ..registerBuilder<DeleteQuestionUseCase>(
+      (DependencyInjection di) => DeleteQuestionUseCase(
+        repositoryFactory: di.get<RepositoryFactory>(),
+      ),
+    )
+    ..registerBuilder<GetSingleQuestionUseCase>(
+      (DependencyInjection di) => GetSingleQuestionUseCase(
+        questionRepository: di.get<QuestionRepository>(),
+      ),
+    )
     ..registerBuilder<UseCaseFactory>(
-      (DependencyInjection di) =>
-          UseCaseFactory(repositoryFactory: di.get<RepositoryFactory>()),
+      (DependencyInjection di) => UseCaseFactory(
+        watchAllQuestionsUseCase: di.get<WatchAllQuestionsUseCase>(),
+        createQuestionUseCase: di.get<CreateQuestionUseCase>(),
+        deleteQuestionUseCase: di.get<DeleteQuestionUseCase>(),
+        getSingleQuestionUseCase: di.get<GetSingleQuestionUseCase>(),
+        updateQuestionUseCase: di.get<UpdateQuestionUseCase>(),
+      ),
     )
     ..registerBuilder<PresentationMapperFactory>(
       (di) => PresentationMapperFactory(),
