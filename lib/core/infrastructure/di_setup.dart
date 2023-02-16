0805fd5040337812b0a358c056d31113bf86da7d
gabriel <gabsvander@hotmail.com>
2023-02-15 21:35:07 -0300

    QL-31 Removed unused factory

--- lib/core/infrastructure/di_setup.dart
+++ lib/core/infrastructure/di_setup.dart
@@ -1,7 +1,6 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:quiz_lab/core/data/data_sources/appwrite_data_source.dart';
 import 'package:quiz_lab/core/presentation/manager/bottom_navigation/bottom_navigation_cubit.dart';
-import 'package:quiz_lab/core/presentation/manager/factories/core_cubit_factory.dart';
 import 'package:quiz_lab/core/presentation/manager/network/network_cubit.dart';
 import 'package:quiz_lab/core/utils/dependency_injection/dependency_injection.dart';
 
@@ -12,11 +11,5 @@ void coreDependencyInjectionSetup(DependencyInjection di) {
       (di) => AppwriteDataSource(appwriteAccountService: di.get<Account>()),
     )
     ..registerFactory<NetworkCubit>((_) => NetworkCubit())
-    ..registerFactory<BottomNavigationCubit>((_) => BottomNavigationCubit())
-    ..registerBuilder<CoreCubitFactory>(
-      (di) => CoreCubitFactory(
-        networkCubit: di.get<NetworkCubit>(),
-        bottomNavigationCubit: di.get<BottomNavigationCubit>(),
-      ),
-    );
+    ..registerFactory<BottomNavigationCubit>((_) => BottomNavigationCubit());
 }
