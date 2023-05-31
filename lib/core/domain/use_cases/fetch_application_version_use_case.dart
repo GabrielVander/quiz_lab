8e9afbbf506c91ec5fb9537f0fbb1aea61f23d50
gabriel <gabsvander@hotmail.com>
2023-05-31 11:47:46 -0300

    QL-56 Added logging statement to FetchApplicationVersionUseCase

--- lib/core/domain/use_cases/fetch_application_version_use_case.dart
+++ lib/core/domain/use_cases/fetch_application_version_use_case.dart
@@ -1,3 +1,5 @@
+import 'package:quiz_lab/core/utils/logger/impl/quiz_lab_logger_factory.dart';
+import 'package:quiz_lab/core/utils/logger/quiz_lab_logger.dart';
 import 'package:quiz_lab/core/wrappers/package_info_wrapper.dart';
 
 // ignore: one_member_abstracts
@@ -9,8 +11,14 @@ class FetchApplicationVersionUseCaseImpl implements FetchApplicationVersionUseCa
   FetchApplicationVersionUseCaseImpl({required PackageInfoWrapper packageInfoWrapper})
       : _packageInfoWrapper = packageInfoWrapper;
 
+  final QuizLabLogger _logger =
+      QuizLabLoggerFactory.createLogger<FetchApplicationVersionUseCaseImpl>();
   final PackageInfoWrapper _packageInfoWrapper;
 
   @override
-  String execute() => _packageInfoWrapper.applicationVersion;
+  String execute() {
+    _logger.debug('Executing...');
+
+    return _packageInfoWrapper.applicationVersion;
+  }
 }
